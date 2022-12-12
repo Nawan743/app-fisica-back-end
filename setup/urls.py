9ac5d72af410afea2476fce0c51233c290c79834
Nawan743 <nawandx743@gmail.com>
2022-12-12 11:52:57 -0300

    Criação de modulo de login
    - Criação de endpoint para login
    - Remoção de linha desnecessária
    - Adição de urls e modulo ao core

--- setup/urls.py
+++ setup/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include('register.urls'))
+    path("", include('register.urls')),
+    path("", include('login.urls'))
 ]
